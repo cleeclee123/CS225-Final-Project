329e3ba9365c33349e48a28b9a001a01a6d02161
Chris Lee <cl114@illinois.edu>
2022-11-20 21:22:20 +0000

    updated airline header

--- project/src/airline.h
+++ project/src/airline.h
@@ -6,11 +6,33 @@
 class Airline
 {
 public:
+  /**
+   * @brief Default Constructor of new Airline object
+   *
+   */
   Airline();
-  Airline(std::string name, std::vector<std::string> knownAbbreviatations, std::string country);
+
+  /**
+   * @brief Parameterized Constructor of new Airline object
+   * 
+   * @param airlineID 
+   * @param name 
+   * @param alias 
+   * @param IATA 
+   * @param ICAO 
+   * @param callsign 
+   * @param country 
+   * @param isActive 
+   */
+  Airline(std::string airlineID, std::string name, std::string alias, unsigned IATA, unsigned ICAO, std::string callsign, std::string country, bool isActive);
 
 private:
-  std::string name_; 
-  std::vector<std::string> knownAbbreviatations_;
+  std::string airlineID_;
+  std::string name_;
+  std::string alias_;
+  unsigned IATA_;
+  unsigned ICAO_;
+  std::string callsign_;
   std::string country_;
+  bool isActive_;
 };
