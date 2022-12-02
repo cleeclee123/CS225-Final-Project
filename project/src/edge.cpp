05e82793ad0f3d1df7dab43b49f24b5dd5bdab99
Chris Lee <cl114@illinois.edu>
2022-12-02 21:50:26 +0000

    kaka error

--- project/src/edge.cpp
+++ project/src/edge.cpp
@@ -25,11 +25,11 @@ void Edge::printEdge()
   std::cout << "airline: " << airlineID_ << std::endl;
   std::cout << "airline id " << airlineID_ << std::endl;
   std::cout << "source airport iata" << srcIATA_ << std::endl;
-  std::cout << "source airport id " << srcID_ _ << std::endl;
-  std::cout << "dest airport iata " << destIATA_ _ << std::endl;
-  std::cout << "dest airport id " << destID_ _ << std::endl;
-  std::cout << "does codeshares " << codeshare_ _ << std::endl;
-  std::cout << "number of stops " << stops_ _ << std::endl;
+  std::cout << "source airport id " << srcID_ << std::endl;
+  std::cout << "dest airport iata " << destIATA_ << std::endl;
+  std::cout << "dest airport id " << destID_ << std::endl;
+  std::cout << "does codeshares " << codeshare_ << std::endl;
+  std::cout << "number of stops " << stops_ << std::endl;
   std::cout << "planetypes " << planeTypes_ << std::endl;
   
 }
