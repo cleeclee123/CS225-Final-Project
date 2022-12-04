465fc80b330908609681775292c35fc3d0b2788c
Chris Lee <cl114@illinois.edu>
2022-12-04 02:30:21 +0000

    added utils for graph construction

--- project/src/utils.cpp
+++ project/src/utils.cpp
@@ -3,10 +3,26 @@
 #include "airline.h"
 #include <string>
 
-Airport getAirportByIATA(std::vector<Airport> airports, std::string IATA) {
-
+Airport getAirportByIATA(std::vector<Airport> airports, std::string IATA)
+{
+  for (const auto &airport : airports)
+  {
+    if (airport.IATA_ == IATA)
+    {
+      return airport;
+    }
+  }
+  return Airport();
 }
 
-Airline getAirlineByIATA(std::vector<Airline> airlines, std::string IATA) {
-
+Airline getAirlineByIATA(std::vector<Airline> airlines, std::string IATA)
+{
+  for (const auto &airline : airlines)
+  {
+    if (airline.IATA_ == IATA)
+    {
+      return airline;
+    }
+  }
+  return Airline();
 }
