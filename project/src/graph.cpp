b2245d4c56b7109195d3be1bb222a5fe3e7454fe
Chris Lee <cl114@illinois.edu>
2022-12-06 03:28:55 +0000

    updated graph with new edge class

--- project/src/graph.cpp
+++ project/src/graph.cpp
@@ -1,67 +1,101 @@
 #include "graph.h"
 #include "airport.h"
+#include "utils.h"
 #include <iostream>
 #include <vector>
 #include <map>
 #include <string>
 
-void Graph::addEdge(Airport from, Airport to) {
-    Edge edge;
-    edge.from_ = from;
-    edge.to_ = to;
-    edge.distance = from.distance(to);
-    if (std::find(adjList_.begin(), adjList_.end(), from) == adjList_.end()) {
+void Graph::addEdge(Airport from, Airport to, Edge current)
+{
+    if (std::find(adjList_.begin(), adjList_.end(), from) == adjList_.end())
+    {
         addAirport(from);
-    } 
-    adjList_[from].push_back(edge);
-    
+    }
+    adjList_[from].push_back(current);
 }
 
-void Graph::addAirport(Airport airport) {
+void Graph::addAirport(Airport airport)
+{
     adjList_[airport] = std::vector<Edge>();
 }
 
-void Graph::setStartingAirport(Airport airport) {
+void Graph::setStartingAirport(Airport airport)
+{
     startingAirport_ = airport;
 }
 
-std::vector<Airport> Graph::getAirports() {
+std::vector<Airport> Graph::getAirports()
+{
     std::vector<Airport> airports;
-    for (auto it = adjList_.begin(); it != adjList_.end(); it++) {
+    for (auto it = adjList_.begin(); it != adjList_.end(); it++)
+    {
         airports.push_back(it->first);
     }
     return airports;
 }
 
-std::vector<Airport> Graph::getAdjacentAirports(Airport airport) {
+std::vector<Airport> Graph::getAdjacentAirports(Airport airport)
+{
     std::vector<Airport> airports;
-    for (auto it = adjList_[airport].begin(); it != adjList_[airport].end(); it++) {
-        airports.push_back(it->to_);
+    for (auto it = adjList_[airport].begin(); it != adjList_[airport].end(); it++)
+    {
+        airports.push_back(getAirportByIATA(it->destIATA_));
     }
     return airports;
 }
 
-double Graph::getWeight(Airport from, Airport to) {
+double Graph::getWeight(Airport from, Airport to)
+{
     return from.distance(to);
 }
 
-double Graph::Dijkstra(Graph graph, Airport from, Airport to) {
+Airport Graph::getAirportByIATA(std::string IATA)
+{
+    for (const auto &airport : airports_)
+    {
+        if (airport.IATA_ == IATA)
+        {
+            return airport;
+        }
+    }
+    return Airport();
+}
+
+Airline Graph::getAirlineByIATA(std::string IATA)
+{
+    for (const auto &airline : airlines_)
+    {
+        if (airline.IATA_ == IATA)
+        {
+            return airline;
+        }
+    }
+    return Airline();
+}
+
+double Graph::Dijkstra(Graph graph, Airport from, Airport to)
+{
     std::priority_queue<std::pair<double, Airport>, std::vector<std::pair<double, Airport>>, std::greater<std::pair<double, Airport>>> pq;
     std::map<Airport, double> dist;
     std::vector<Airport> airports = graph.getAirports();
-    for (auto it = airports.begin(); it != airports.end(); it++) {
+    for (auto it = airports.begin(); it != airports.end(); it++)
+    {
         dist[*it] = INT_MAX;
     }
     dist[from] = 0;
     pq.push(std::make_pair(0, from));
-    while (!pq.empty()) {
+    while (!pq.empty())
+    {
         Airport u = pq.top().second;
         pq.pop();
         std::vector<Airport> adj = graph.getAdjacentAirports(u);
-        for (auto it = adj.begin(); it != adj.end(); it++) {
+        for (auto it = adj.begin(); it != adj.end(); it++)
+        {
             Airport v = *it;
             double weight = graph.getWeight(u, v);
-            if (dist[v] > dist[u] + weight) {
+            if (dist[v] > dist[u] + weight)
+            {
                 dist[v] = dist[u] + weight;
                 pq.push(std::make_pair(dist[v], v));
             }
@@ -69,4 +103,3 @@ double Graph::Dijkstra(Graph graph, Airport from, Airport to) {
     }
     return dist[to];
 }
-
--- project/src/graph.h
+++ project/src/graph.h
@@ -4,19 +4,15 @@
 #include <map>
 #include <string>
 #include <bits/stdc++.h>
+
 #include "airport.h"
+#include "edge.h"
 
-struct Edge {
-  Airport from_;
-  Airport to_;
-  std::string uid_ = from_.IATA_ + to_.IATA_;
-  double distance = from_.distance(to_);
-};
-
-class Graph {
-  public: 
-  // methods: 
-  /*  
+class Graph
+{
+public:
+  // methods:
+  /*
     // for graph construction
     - add edge
     - add vertex
@@ -28,15 +24,21 @@ class Graph {
     - get adjacent vertices
     - get weight of edge
   */
-    void addEdge(Airport from, Airport to);
-    void addAirport(Airport airport);
-    void setStartingAirport(Airport airport);
-    std::vector<Airport> getAirports();
-    std::vector<Airport> getAdjacentAirports(Airport airport);
-    double getWeight(Airport from, Airport to);
-    double Dijkstra(Graph graph, Airport from, Airport to);
+  Graph();
+  void Graph::addEdge(Airport from, Airport to, Edge current);
+  void addAirport(Airport airport);
+  void setStartingAirport(Airport airport);
+  std::vector<Airport> getAirports();
+  std::vector<Airport> getAdjacentAirports(Airport airport);
+  double getWeight(Airport from, Airport to);
+  double Dijkstra(Graph graph, Airport from, Airport to);
+  Airport getAirportByIATA(std::string IATA);
+  Airline getAirlineByIATA(std::string IATA);
 
-  private: 
-    Airport startingAirport_;
-    std::map<Airport, std::vector<Edge>> adjList_;
+private:
+  Airport startingAirport_;
+  std::map<Airport, std::vector<Edge>> adjList_;
+  std::vector<Airport> airports_;
+  std::vector<Airline> airlines_;
+  std::vector<Edge> routes_;
 };
