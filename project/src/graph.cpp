aec967b8e0b79cf926d8fd9cdd915be9d3a3191c
Chris Lee <cl114@illinois.edu>
2022-12-06 03:41:56 +0000

    spaces over tabs

--- project/src/graph.cpp
+++ project/src/graph.cpp
@@ -6,75 +6,76 @@
 #include <map>
 #include <string>
 
-Graph::Graph(std::vector<Airport> airports, std::vector<Airline> airlines) {
-    airports_ = airports;
-    airlines_ = airlines;
+Graph::Graph(std::vector<Airport> airports, std::vector<Airline> airlines)
+{
+  airports_ = airports;
+  airlines_ = airlines;
 }
 
 void Graph::addEdge(Airport from, Airport to, Edge current)
 {
-    if (std::find(adjList_.begin(), adjList_.end(), from) == adjList_.end())
-    {
-        addAirport(from);
-    }
-    adjList_[from].push_back(current);
+  if (std::find(adjList_.begin(), adjList_.end(), from) == adjList_.end())
+  {
+    addAirport(from);
+  }
+  adjList_[from].push_back(current);
 }
 
 void Graph::addAirport(Airport airport)
 {
-    adjList_[airport] = std::vector<Edge>();
+  adjList_[airport] = std::vector<Edge>();
 }
 
 void Graph::setStartingAirport(Airport airport)
 {
-    startingAirport_ = airport;
+  startingAirport_ = airport;
 }
 
 std::vector<Airport> Graph::getAirports()
 {
-    std::vector<Airport> airports;
-    for (auto it = adjList_.begin(); it != adjList_.end(); it++)
-    {
-        airports.push_back(it->first);
-    }
-    return airports;
+  std::vector<Airport> airports;
+  for (auto it = adjList_.begin(); it != adjList_.end(); it++)
+  {
+    airports.push_back(it->first);
+  }
+  return airports;
 }
 
 std::vector<Airport> Graph::getAdjacentAirports(Airport airport)
 {
-    std::vector<Airport> airports;
-    for (auto it = adjList_[airport].begin(); it != adjList_[airport].end(); it++)
-    {
-        airports.push_back(getAirportByIATA(it->destIATA_));
-    }
-    return airports;
+  std::vector<Airport> airports;
+  for (auto it = adjList_[airport].begin(); it != adjList_[airport].end(); it++)
+  {
+    airports.push_back(getAirportByIATA(it->destIATA_));
+  }
+  return airports;
 }
 
 double Graph::getWeight(Airport from, Airport to)
 {
-    return from.distance(to);
+  return from.distance(to);
 }
 
 Airport Graph::getAirportByIATA(std::string IATA)
 {
-    for (const auto &airport : airports_)
+  for (const auto &airport : airports_)
+  {
+    if (airport.IATA_ == IATA)
     {
-        if (airport.IATA_ == IATA)
-        {
-            return airport;
-        }
+      return airport;
     }
-    return Airport();
+  }
+  return Airport();
 }
 
 Airline Graph::getAirlineByIATA(std::string IATA)
 {
-    for (const auto &airline : airlines_)
+  for (const auto &airline : airlines_)
+  {
+    if (airline.IATA_ == IATA)
     {
-        if (airline.IATA_ == IATA)
-        {
-            return airline;
-        }
+      return airline;
     }
-    return Airline();
+  }
+  return Airline();
 }
