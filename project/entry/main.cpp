b9a2f5256406a21c3a2d0de85e3b00a4c6dea33a
Chris Lee <cl114@illinois.edu>
2022-12-06 04:09:10 +0000

    building big ass graph in main slow af tho

--- project/entry/main.cpp
+++ project/entry/main.cpp
@@ -2,21 +2,22 @@
 
 #include "graph.h"
 #include "parser.h"
-#include "utils.h"
 
 int main()
 {
     std::vector<Airport> airports = parseAirports("../tests/data/airports.dat");
     std::vector<Airline> airlines = parseAirlines("../tests/data/airlines.dat");
     std::vector<Edge> routes = parseRoutes("../tests/data/routes.dat");
-    
-    Graph graph;
-    
-    for (const auto& route : routes) {
-        Airport src = getAirportByIATA(airports, route.srcIATA_);
-        Airport dest = getAirportByIATA(airports, route.destIATA_);
+
+    Graph graph(airports, airlines);
+
+    for (const auto &route : routes)
+    {
+        Airport src = graph.getAirportByIATA(route.srcIATA_);
+        Airport dest = graph.getAirportByIATA(route.destIATA_);
         graph.addEdge(src, dest, route);
     }
+    graph.printGraph();
 
     return 0;
 }
